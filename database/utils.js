91760d5687b9cca38fcd80b66bd6a1a416dd3f4f
Josiah Liciaga <35929809+Jliciaga@users.noreply.github.com>
2018-08-15 14:34:45 -0400

    database functional, added testing, added utilities

--- database/utils.js
+++ database/utils.js
@@ -157,38 +157,3 @@ exports.saveCategory = category =>
 //       location: location.location_coordinates,
 //       }).fetch().then(save => (save ? reject() : Locations.create(location).then(resolve)))
 //   })
-=======
-const db = require('./config.js');
-const Users = require('./collections/users.js');
-const User = require('./models/users.js');
-const Categories = require('./collections/category.js');
-const Category = require('./models/category.js');
-const Events = require('./collections/event.js');
-const Event = require('./models/event.js');
-
-//export saves user to database input fields are (username, password)
-exports.saveUser = user => {
-    new Promise({function (resolve, reject) {
-        new User({ username: user.username }).fetch().then(save => (save ? reject() : Users.create(user).then(resolve)));
-    }})
-}
-
-//Get all information of the user that is currently logged in without a password for security purposes.
-exports.getUser = (username) => {
-    new Promise(resolve => new User({ username: username }).fetch().then(found => resolve(delete found.attributes.password && found.attributes)))
-}
-
-//export saves event to database input fields are ()
-exports.saveEvent = event => {
-    new Promise({function (resolve, reject) {
-        new Event({event_name: event.event_name }).fetch().then(save => (save ? reject() : Events.create(event).then(resolve)));
-    }})
-}
-
-//export saves category to database input fields are ()
-exports.saveCategory = category => {
-    Promise({function (resolve, reject) {
-        new Category({event_name: event.event_name }).fetch().then(save => (save ? reject() : Categories.create(category).then(resolve)));
-    }})
-}
->>>>>>> Changed database structure
